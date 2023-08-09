c0fd9570d2292ac211f1a11095b0210398919a35
Eddie Wynman <edward.wynman@gmail.com>
2023-08-09 12:29:39 -0400

    Test Commit Script

--- commit.sh
+++ commit.sh
@@ -4,16 +4,37 @@
 echo "Enter commit message:"
 read message
 
-# Run build
+# Navigate to the frontend directory
+cd frontend
+
+# Run build for the React app
 npm run build
 
-# Add all files to staging area
-git add .
+# Go back to the root directory
+cd ..
 
-# Commit changes with the specified message
-git commit -m "$message"
+# Navigate to the backend directory
+cd backend
 
-# Push changes to the remote repository
-git push origin master
+# Run build for the Express app
+npm run build
 
-echo "Changes pushed to remote repository."
+# Go back to the root directory
+cd ..
+
+# Check for changes
+if git diff-index --quiet HEAD --; then
+    echo "No changes to commit."
+else
+    # Add all files to staging area
+    git add .
+
+    # Commit changes with the specified message
+    git commit -m "$message"
+
+    # Push changes to the current branch
+    current_branch=$(git branch --show-current)
+    git push origin "$current_branch"
+
+    echo "Changes pushed to remote repository."
+fi
