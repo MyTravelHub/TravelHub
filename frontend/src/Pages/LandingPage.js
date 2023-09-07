6e3d0ea469ec743277b837292b46f85d646c2308
Eddie Wynman <edward.wynman@gmail.com>
2023-09-07 17:17:12 -0400

    Weather Data, Real Time FLight Search, CSS

--- frontend/src/Pages/LandingPage.js
+++ frontend/src/Pages/LandingPage.js
@@ -1,7 +1,32 @@
-// React Imports
-import React from "react";
+// // React Imports
+// import React from "react";
 
-// Style Imports
+// // Style Imports
+// import "../CSS/LandingPage.css";
+
+// // Component Imports
+// import Navbar from "../Components/Navbar";
+// import WeatherDisplay from "../Components/HomePage/WeatherDisplay";
+// import MainHomeContainer from "../Components/HomePage/MainContainer";
+// import Footer from "../Components/Footer";
+
+// // Page Component
+// const LandingPage = () => {
+//   return (
+//     <div>
+//       <Navbar />
+//       <MainHomeContainer />
+//       <WeatherDisplay />
+//       <Footer/>
+//     </div>
+//   );
+// };
+
+// export default LandingPage;
+
+// LandingPage.js
+
+import React, { useState } from "react";
 import "../CSS/LandingPage.css";
 
 // Component Imports
@@ -10,14 +35,30 @@ import WeatherDisplay from "../Components/HomePage/WeatherDisplay";
 import MainHomeContainer from "../Components/HomePage/MainContainer";
 import Footer from "../Components/Footer";
 
-// Page Component
 const LandingPage = () => {
+  const [showModal, setShowModal] = useState(true);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
       <Navbar />
       <MainHomeContainer />
       <WeatherDisplay />
-      <Footer/>
+      <Footer />
+
+      {showModal && (
+        <div className="popup-modal">
+          <div className="modal-content">
+            <p>Still in development, some features may not work.</p>
+            <button className="close-button" onClick={handleCloseModal}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
